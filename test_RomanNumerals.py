80f1d6078c5bc67b8665df85c6d7ca86a82edc6e
Samrhan <samuel.bader83@gmail.com>
2021-06-08 10:54:44 +0200

    add a test

--- test_RomanNumerals.py
+++ test_RomanNumerals.py
@@ -5,6 +5,9 @@ def test_basic_numerals():
     output = to_roman_numerals(1)
     assert output == 'I'
 
+def test_basic_numerals_1000():
+    output = to_roman_numerals(1000)
+    assert output == 'M'
 
 def test_complex_numerals():
     output = to_roman_numerals(1246)
