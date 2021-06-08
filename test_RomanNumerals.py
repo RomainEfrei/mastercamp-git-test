6fa255bc16dadb4e0d37f7b70eff36ab885d6f00
Samrhan <samuel.bader83@gmail.com>
2021-06-08 11:05:08 +0200

    reformat code

--- test_RomanNumerals.py
+++ test_RomanNumerals.py
@@ -5,9 +5,11 @@ def test_basic_numerals():
     output = to_roman_numerals(1)
     assert output == 'I'
 
+
 def test_basic_numerals_1000():
     output = to_roman_numerals(1000)
     assert output == 'M'
+
 
 def test_complex_numerals():
     output = to_roman_numerals(1246)
